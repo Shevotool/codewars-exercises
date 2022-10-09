bd59216f76361a4ad936658f16d65428393b561f
Andrés González <89161075+Shevotool@users.noreply.github.com>
2022-10-09 02:44:24 -0600

    Tercer Commit

---
+++ js/make-uppercase.js
@@ -0,0 +1,8 @@
+/* 8 kyu MakeUpperCase */
+
+/* Write a function which converts the input string to uppercase. */
+
+function makeUpperCase(str) {
+  let name = str.toUpperCase();
+  return name;
+}
